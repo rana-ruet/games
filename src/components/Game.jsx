8a77327fd22cd8374e6dd89957638487e78a92e8
Rana <rana.ruet16@gmail.com>
2023-02-15 20:19:41 +0600

    show the game component

---
+++ src/components/Game.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Game = ({ gameData }) => {
+  return (
+    <StyledGame>
+      <h3>{gameData.name}</h3>
+      <p>{gameData.released}</p>
+      <img src={gameData.background_image} alt={gameData.name} />
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    width: 100%;
+
+    object-fit: cover;
+  }
+`;
+export default Game;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
 import { loadGames } from '../actions/gamesAction';
+import Game from '../components/Game';
 
 function Home() {
   const dispatch = useDispatch();
@@ -9,7 +13,32 @@ function Home() {
     dispatch(loadGames());
   }, []);
 
-  return <h1>Hi From Home</h1>;
+  const { popular, upcoming, newGames } = useSelector((state) => state.game);
+
+  return (
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game key={game.id} gameData={game} />
+        ))}
+      </Games>
+    </GameList>
+  );
 }
 
 export default Home;
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
